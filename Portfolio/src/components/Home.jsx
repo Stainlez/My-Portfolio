628fbbfb7b1886f0536bc81fdc42d870012615eb
unknown <ezeakaluejudejnr@gmail.com>
2024-01-08 07:26:27 -0800

    editted home file

--- Portfolio/src/components/Home.jsx
+++ Portfolio/src/components/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
             </button>
             <nav className="nav">
                 <ul className="nav__list">
-                    <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
+                    <li className="nav__item"><a href="#home" className="nav__link">ourrrrrr Home</a></li>
                     <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
                     <li className="nav__item"><a href="#about" className="nav__link">About me</a></li>
                     <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
